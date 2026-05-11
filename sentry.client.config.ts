88ecef071fbcced6f0210ab5781c5733949bde19
Joan Martorell Coll <joanmartorellcoll03@gmail.com>
2026-05-11 12:58:56 +0200

    fix(#11): harden Sentry client (mask PII, lower traces, scrub headers)
    - sendDefaultPii: false (no email/IP/user-agent)
    - tracesSampleRate: 0.1 (was 1.0)
    - replayIntegration with maskAllInputs/maskAllText/blockAllMedia
    - beforeSend strips cookies, Authorization and cookie headers

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -7,25 +7,46 @@ Sentry.init({
 
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.1,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
   replaysSessionSampleRate: 0.1,
-  
+
   // If the entire session is not sampled, use the below sample rate to sample
   // sessions when an error occurs.
   replaysOnErrorSampleRate: 1.0,
-  
-  // If you don't want to use Session Replay, just remove the line below:
-  integrations: [Sentry.replayIntegration()],
+
+  // Session Replay with PII masking: inputs, text and media are scrubbed
+  // before leaving the browser so passwords and personal data never reach Sentry.
+  integrations: [
+    Sentry.replayIntegration({
+      maskAllInputs: true,
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
+  ],
 
   // Enable logs to be sent to Sentry
   enableLogs: true,
 
-  // Enable sending of user PII (Personally Identifiable Information)
+  // Do not send user PII (email, IP, user-agent) by default.
   // https://docs.sentry.io/platforms/javascript/guides/nuxt/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
+  sendDefaultPii: false,
+
+  // Strip auth/cookie material from every outgoing event as a last-line defense.
+  beforeSend(event) {
+    if (event.request) {
+      delete event.request.cookies;
+      if (event.request.headers) {
+        delete event.request.headers["Authorization"];
+        delete event.request.headers["authorization"];
+        delete event.request.headers["cookie"];
+        delete event.request.headers["Cookie"];
+      }
+    }
+    return event;
+  },
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
